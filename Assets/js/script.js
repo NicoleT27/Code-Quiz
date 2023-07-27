d620643f7fda766e28bce32d8f267a449a704adc
Nicole <nicolejoan9@yahoo.com>
2023-07-27 17:55:34 -0400

    added time interval and multiple choice array

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,33 +1,65 @@
 var button1 = document.querySelector("#startBtn");
-let questions = document.querySelector("#questions");
-let answers = document.querySelector("#answers");
+let header = document.querySelector("#header");
+let subHeading = document.querySelector("#subHeading");
 
 function startQuiz() {
-  questions.setAttribute(
+  header.setAttribute(
     "style",
-    "font-size: 45px; text-align: center; font-weight:bold; margin-top:200px;"
+    "font-size: 30px; text-align: center; font-weight:bold; margin-top:200px;"
   );
-  answers.setAttribute(
+  subHeading.setAttribute(
     "style",
     "font-size: 45px; text-align: center; font-weight:bold; margin-top:200px;"
   );
 
   button1.setAttribute("style", "display: none;");
-  questions.innerText = "What does CSS stand for?";
-  answers.innerText = null;
+  subHeading.innerText = null;
+  startTime();
+  showQuestion();
+}
+
+function startTime() {
+  var minutesLeft = 20;
+  var timer = setInterval(function () {
+    document.querySelector("#timeId").innerHTML = minutesLeft;
+    minutesLeft--;
+    if (minutesLeft <= 0) {
+      clearInterval(timer);
+    }
+  }, 60000);
 }
 
 startBtn.addEventListener("click", startQuiz);
 
-function question1() {
-  let button2 = document.createElement("button2");
-  button2.innerText = "Can you click me?";
-  document.querySelector("button2").setAttribute("style", "margin-left:550px;");
-  document.body.appendChild(button2);
+function showQuestion() {
+  var multiChoice = [
+    {
+      title: "How many letters in JS?",
+      choices: ["1", "2", "3", "4"],
+      correctA: "2",
+    },
+    {
+      title: "How many letters in CSS?",
+      choices: ["1", "2", "3", "4"],
+      correctA: "3",
+    },
+    {
+      title: "How many letters in HTML?",
+      choices: ["1", "2", "3", "4"],
+      correctA: "4",
+    },
+  ];
 
-  if (button2.style.display === "none") {
-    button2.setAttribute("style", "display:block;");
-  } else {
-    button2.setAttribute("style", "display:none;");
+  for (let i = 0; i < multiChoice.length; i++) {
+    var questionsTitle = document.getElementById("#questionTitle");
+    questionsTitle.innerHTML = showQuestion.title;
+
+    if (questions === "hide") {
+      questions.setAttribute("class", "show");
+      questions = show;
+    } else {
+      questions = hide;
+    }
   }
 }
+showQuestion();
