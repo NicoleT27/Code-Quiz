eac2f1ee280d36005e08e9385c519139dfc1e4d9
Nicole <nicolejoan9@yahoo.com>
2023-08-02 12:36:45 -0400

    updated time interval, added ability to clear highscores,displayed highscores and fixed styling layout

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -10,6 +10,7 @@ var leaderboardEl = document.getElementById("leaderboard");
 var yourScore = document.getElementById("yourScore");
 var scoreLink = document.getElementById("scoreLink");
 var button = document.createElement("button");
+var endPage = document.getElementById("endPage");
 var counter = 0;
 var score = 0;
 var timerEnd;
@@ -22,7 +23,7 @@ function startQuiz() {
   showQuestion();
 }
 
-var startingMinutes = 10;
+var startingMinutes = 5;
 var time = startingMinutes * 60;
 var multiChoice = [
   {
@@ -37,75 +38,75 @@ var multiChoice = [
     choices: ["If-Else", "For", "While", "If"],
     correctA: "For",
   },
-  // {
-  //   title: "A loop that never ends is referred to as a(n)_________.?",
-  //   choices: ["While loop", "Infinite loop", "Recursive loop", "For loop"],
-  //   correctA: "Infinite loop",
-  // },
-  // {
-  //   title:
-  //     "What is the term used for a block of code that is executed repeatedly until a certain condition is met?",
-  //   choices: ["Function", "Loop", "Condition", "Variable"],
-  //   correctA: "Loop",
-  // },
-  // {
-  //   title:
-  //     "Which data structure is used for storing a collection of elements in a non-linear fashion?",
-  //   choices: ["Array", "Stack", "Queue", "Tree"],
-  //   correctA: "Tree",
-  // },
-  // {
-  //   title: "Which of the following is not a web browser?",
-  //   choices: ["Chrome", "Safari", "Firefox", "Photoshop"],
-  //   correctA: "Photoshop",
-  // },
-  // {
-  //   title: "Which of the following is not a programming language?",
-  //   choices: ["HTML", "CSS", "MySQL", "JavaScript"],
-  //   correctA: "MySQL",
-  // },
-  // {
-  //   title: "Which of the following is a type of computer network?",
-  //   choices: [
-  //     "Local Area Network (LAN)",
-  //     "Wide Area Network (WAN)",
-  //     "Metropolitan Area Network (MAN)",
-  //     "All of the above",
-  //   ],
-  //   correctA: "All of the above",
-  // },
-  // {
-  //   title: "During program development, software requirements specify?",
-  //   choices: [
-  //     " How the program will accomplish the task",
-  //     "What the task is that the program will perform.",
-  //     "How to divide the task into subtasks",
-  //     "How to test the program when it is done",
-  //   ],
-  //   correctA: "What the task is that the program will perform.",
-  // },
-  // {
-  //   title: "What does HTML stand for?",
-  //   choices: [
-  //     "Hyper Trainer Marking Language",
-  //     "Hyper Text Marketing Language",
-  //     "Hyper Text Markup Language",
-  //     "Hyper Text Markup Leveler",
-  //   ],
-  //   correctA: "Hyper Text Markup Language",
-  // },
+  {
+    title: "A loop that never ends is referred to as a(n)_________.?",
+    choices: ["While loop", "Infinite loop", "Recursive loop", "For loop"],
+    correctA: "Infinite loop",
+  },
+  {
+    title:
+      "What is the term used for a block of code that is executed repeatedly until a certain condition is met?",
+    choices: ["Function", "Loop", "Condition", "Variable"],
+    correctA: "Loop",
+  },
+  {
+    title:
+      "Which data structure is used for storing a collection of elements in a non-linear fashion?",
+    choices: ["Array", "Stack", "Queue", "Tree"],
+    correctA: "Tree",
+  },
+  {
+    title: "Which of the following is not a web browser?",
+    choices: ["Chrome", "Safari", "Firefox", "Photoshop"],
+    correctA: "Photoshop",
+  },
+  {
+    title: "Which of the following is not a programming language?",
+    choices: ["HTML", "CSS", "MySQL", "JavaScript"],
+    correctA: "MySQL",
+  },
+  {
+    title: "Which of the following is a type of computer network?",
+    choices: [
+      "Local Area Network (LAN)",
+      "Wide Area Network (WAN)",
+      "Metropolitan Area Network (MAN)",
+      "All of the above",
+    ],
+    correctA: "All of the above",
+  },
+  {
+    title: "During program development, software requirements specify?",
+    choices: [
+      " How the program will accomplish the task",
+      "What the task is that the program will perform.",
+      "How to divide the task into subtasks",
+      "How to test the program when it is done",
+    ],
+    correctA: "What the task is that the program will perform.",
+  },
+  {
+    title: "What does HTML stand for?",
+    choices: [
+      "Hyper Trainer Marking Language",
+      "Hyper Text Marketing Language",
+      "Hyper Text Markup Language",
+      "Hyper Text Markup Leveler",
+    ],
+    correctA: "Hyper Text Markup Language",
+  },
 ];
 
 function startTime() {
-  var minutes = Math.floor(time / 60);
-  var seconds = time % 60;
-  seconds = seconds < 10 ? "0" + seconds : seconds;
+   var minutes = Math.floor(time / 60);
+   var seconds = time % 60;
+   seconds = seconds < 10 ? "0" + seconds : seconds;
 
-  countdown.innerHTML = ` ${minutes} : ${seconds}`;
-  time--;
-  if (time < 0) {
-    clearInterval(timerEnd);
-  }
+   countdown.innerHTML = ` ${minutes} : ${seconds}`;
+   time--;
+   if (time < 0) {
+     clearInterval(timerEnd);
+   }
 }
 
 startBtn.addEventListener("click", startQuiz);
@@ -162,6 +163,7 @@ function showQuestion() {
 }
 
 function endQuiz() {
+  clearInterval(timerEnd);
   if (score < 0) {
     score = 0;
   }
@@ -180,9 +182,6 @@ function endQuiz() {
   button.innerText = "Submit";
   button.setAttribute("id", "submit");
   document.body.appendChild(button);
-  button.addEventListener("click", function () {
-    leaderboard = submit();
-  });
 
   button.addEventListener("click", submit);
 
@@ -193,6 +192,7 @@ function endQuiz() {
   a.href = "index.html";
   document.body.appendChild(a);
 }
+
 function submit() {
   button.setAttribute("style", "display:none;");
   yourScore.classList.remove("show");
@@ -211,25 +211,24 @@ function submit() {
 
   scoreBoard.push(totalScore);
 
-  localStorage.setItem("score", JSON.stringify(totalScore));
+  localStorage.setItem("totalScore", JSON.stringify(scoreBoard));
 
   var clear = document.getElementById("clear");
   clear.addEventListener("click", function () {
     window.localStorage.clear();
+    highScoreEl.setAttribute("style", "display:none");
   });
 
   leaderDisplay(scoreBoard);
 }
+
 function leaderDisplay(scoreBoard) {
   scoreLink.setAttribute("style", "display:none");
-  var scoreBoard = JSON.parse(localStorage.getItem("score")) || [];
-  console.log(scoreBoard);
+  var scoreBoard = JSON.parse(localStorage.getItem("totalScore")) || [];
 
   for (let i = 0; i < scoreBoard.length; i++) {
-    console.log(scoreBoard);
     var liEl = document.createElement("li");
     liEl.textContent = scoreBoard[i].initals + scoreBoard[i].points;
-    console.log(scoreBoard[i].initals + scoreBoard[i].points);
     highScoreEl.appendChild(liEl);
   }
 }
